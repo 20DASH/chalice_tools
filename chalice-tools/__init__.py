e5e423c6bc6d7a92b99a22bc951c6f8a840d9306
tito guidotti <tito.guidotti@gmail.com>
2025-03-10 17:03:43 -0300

    Muda estrutura

--- chalice-tools/__init__.py
+++ chalice-tools/__init__.py
@@ -1,21 +1,2 @@
 
-def no_cors_response(body:dict, status_code=200):
-    from chalice import Response
-    return Response(body=body, status_code=status_code,  headers={
-        'Access-Control-Allow-Origin': '*', 
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE'
-    })
-
-def load_chalice_config(stage="dev"):
-    import json
-    import os
-    with open(".chalice/config.json", "r") as config_file:
-        config = json.load(config_file)
-
-    for key, value in config.get('environment_variables', {}).items():
-        os.environ[key] = value
-
-    stage_env_vars = config.get('stages', {}).get(stage, {}).get('environment_variables', {})
-    for key, value in stage_env_vars.items():
-        os.environ[key] = value
+from chalice_tools import *
--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="chalice_tools",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "chalice"
     ],
     description="Helper tools for development in chalice",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Tito Guidotti",
     author_email="tito.guidotti@gmail.com",
-    url="https://github.com/20DASH/chalice-tools"
+    url="https://github.com/20DASH/chalice_tools",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
